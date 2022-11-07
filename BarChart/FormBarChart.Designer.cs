95b8415a01c0ad84087d7fd34aa7f73bb72d011d
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-07 16:17:56 +0200

    Add project files.

---
+++ BarChart/FormBarChart.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BarChart
+{
+    partial class FormBarChart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnHorizontal = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnVertical = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // BtnHorizontal
+            // 
+            this.BtnHorizontal.Location = new System.Drawing.Point(51, 499);
+            this.BtnHorizontal.Name = "BtnHorizontal";
+            this.BtnHorizontal.Size = new System.Drawing.Size(156, 58);
+            this.BtnHorizontal.TabIndex = 0;
+            this.BtnHorizontal.Text = "Horizontal";
+            this.BtnHorizontal.UseVisualStyleBackColor = true;
+            this.BtnHorizontal.Click += new System.EventHandler(this.BtnHorizontal_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(244, 499);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(147, 58);
+            this.BtnExit.TabIndex = 1;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnVertical
+            // 
+            this.BtnVertical.Location = new System.Drawing.Point(436, 499);
+            this.BtnVertical.Name = "BtnVertical";
+            this.BtnVertical.Size = new System.Drawing.Size(178, 58);
+            this.BtnVertical.TabIndex = 2;
+            this.BtnVertical.Text = "Vertical";
+            this.BtnVertical.UseVisualStyleBackColor = true;
+            this.BtnVertical.Click += new System.EventHandler(this.BtnVertical_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(51, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(747, 462);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // FormBarChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 569);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnVertical);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnHorizontal);
+            this.Name = "FormBarChart";
+            this.Text = "Bar Chart";
+            this.Load += new System.EventHandler(this.FormBarChart_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnHorizontal;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnVertical;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BarChart/FormBarChart.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BarChart
+{
+    public partial class FormBarChart : Form
+    {
+        public FormBarChart()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        
+        private void BtnHorizontal_Click(object sender, EventArgs e)
+        {
+            string[] year = new string[20];
+            double[] sales = new double[20];
+            int counter = 0;
+
+            LoadData(year, sales, ref counter);
+            DisplayHorizontalBarGraph(year, sales, counter);
+            
+        }
+
+        private void LoadData(string[] y, double[] s, ref int c)
+        {
+            string path = Application.StartupPath + @"\sales.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            string line1, line2;
+            bool flag = false;
+
+            while (!flag)
+            {               
+                line1 = streamReader.ReadLine();
+                line2 = streamReader.ReadLine();
+
+                if (line1 == null || line2 == null)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    c++;
+                    y[c] = line1;
+                    s[c] = Convert.ToDouble(line2);
+                }
+            }
+            streamReader.Close();
+        }
+
+        private double FindScalingFactor(double[] s, int c)
+        {
+            double biggest = s[1];
+
+            for (int i = 1; i <= c; i++)
+            {
+                if (s[i] > biggest)
+                    biggest = s[i];
+            }
+
+            double scalingFactor = biggest / (panel1.Width - 200);
+
+            return scalingFactor;
+        }
+
+        private void DisplayHorizontalBarGraph(string[] y, double[] s, int c)
+        {
+            Random r = new Random();
+
+            double sf = FindScalingFactor(s, c);
+
+            g.Clear(panel1.BackColor);
+            Font font = new Font("Verdana", 8);
+            SolidBrush brush = new SolidBrush(Color.Blue);
+
+            g.DrawString("Sales Bar Chart", font, brush, panel1.Width / 2, 10);
+            g.DrawString("Year", font, brush, 10, 45);
+            g.DrawString("Sales", font, brush, 50, 45);
+
+            for (int i = 1; i <= c; i++)
+            {
+                g.DrawString(y[i], font, brush, 10, 48 + i * 30);
+                g.DrawString(s[i].ToString("c"), font, brush, 50, 48 + i * 30);
+
+                int x = (int)(s[i] / sf);
+                Color randomColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                SolidBrush newBrush = new SolidBrush(randomColor);
+                g.FillRectangle(newBrush, 120, 50 + i * 30, x, 10);
+            }
+        }
+
+        private void FormBarChart_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BtnVertical_Click(object sender, EventArgs e)
+        {
+            string[] year = new string[20];
+            double[] sales = new double[20];
+            int counter = 0;
+
+            LoadData(year, sales, ref counter);
+            DisplayVerticalBarGraph(year, sales, counter);
+        }
+
+        private void DisplayVerticalBarGraph(string[] y, double[] s, int c)
+        {
+            Random r = new Random();
+
+            double sf = FindVerticalScalingFactor(s, c);
+
+            g.Clear(panel1.BackColor);
+            Font font = new Font("Verdana", 8);
+            SolidBrush brush = new SolidBrush(Color.Blue);
+
+            g.DrawString("Sales Bar Chart", font, brush, panel1.Width / 2, 10);
+
+            for (int i = 1; i <= c; i++)
+            {
+                g.DrawString(y[i], font, brush, 10 + i * 50, panel1.Height - 50);
+                
+                int yHeight = (int)(s[i] / sf);
+                Color randomColor = Color.FromArgb(r.Next(256), r.Next(256), r.Next(256));
+                SolidBrush newBrush = new SolidBrush(randomColor);
+                g.FillRectangle(newBrush, 20 + i * 50, panel1.Height - 50 - yHeight, 10, yHeight);
+
+                g.DrawString(s[i].ToString("c"), font, brush, i * 50 - 5, panel1.Height - 50 - yHeight - 20);
+            }
+        }
+
+        private double FindVerticalScalingFactor(double[] s, int c)
+        {
+            double biggest = s[1];
+
+            for (int i = 1; i <= c; i++)
+            {
+                if (s[i] > biggest)
+                    biggest = s[i];
+            }
+
+            double scalingFactor = biggest / (panel1.Height - 200);
+
+            return scalingFactor;
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
